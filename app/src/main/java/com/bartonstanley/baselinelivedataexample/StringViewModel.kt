e4d83f24b3c98940b84a2f6e5a9f3450b3e24cc9
Barton Stanley <bstanley@nerdery.com>
2018-12-04 11:07:57 -0600

    removed commented-out code

--- app/src/main/java/com/bartonstanley/baselinelivedataexample/StringViewModel.kt
+++ app/src/main/java/com/bartonstanley/baselinelivedataexample/StringViewModel.kt
@@ -17,6 +17,7 @@ class StringViewModel: ViewModel() {
         get() {
             return StringRepository.observableString
         }
+
     /**
      * Update the value of the string in the Repository.
      *
@@ -25,30 +26,4 @@ class StringViewModel: ViewModel() {
     fun setStringValue(string: String) {
         StringRepository.observableString.value = string
     }
-
-    /*
-    /**
-     * Request to be notified of changes in the length of the string.
-     *
-     * Subscription will remain active for as long as the life cycle owner exists.  Subscription will be paused when
-     * the life cycle owner is paused and will be canceled when the life cycle owner is destroyed.
-     *
-     * @param lifecycleOwner the Activity or Fragment whose life cycle will govern the life cycle of this subscription.
-     * @param onLengthChange lambda that will be invoked once with the initial length of the string and thereafter
-     *        will be invoked when the length of the string changes.  Its parameter is the current length of the string.
-     */
-    fun subscribeToLengthChanges(lifecycleOwner: LifecycleOwner, onLengthChange: (String) -> Unit) {
-
-        // Observer that will publish the new length of the string to subscribers when the string changes
-        val lengthObserver = Observer<String> { newString ->
-            onLengthChange(newString.length.toString())
-        }
-
-        // Attach observer to LiveData object in Repository
-        StringRepository.observableString.observe(lifecycleOwner, lengthObserver)
-
-        // publish initial length to subscribers
-        onLengthChange(StringRepository.string?:"".length.toString())
-    }
-    */
 }
